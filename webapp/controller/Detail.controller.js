9a14e3def1117d0188fd72af6353352c30b4e946
Jefferson Rojas Amador <jefferson.rojas@perceptio.net>
2024-12-04 12:12:24 +0000

    Add Detail view

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+
+    return Controller.extend("student00.com.sap.training.ux402.repository.ux402repository.controller.Detail", {
+        onInit: function () {
+            // Initialization code here
+        }
+
+    });
+});
